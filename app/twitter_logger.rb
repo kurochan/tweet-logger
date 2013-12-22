ee047d0aa9878bbc3b2088ae7d960b916a3922b6
kurochan <kuro@kurochan.org>
2013-12-23 06:18:56 +0900

    切断されても自動で再接続するようにした

--- app/twitter_logger.rb
+++ app/twitter_logger.rb
@@ -16,21 +16,28 @@ class TwitterLogger
 
   def start
     Thread.new do
-      @client = UserStream.client
-      puts 'Start UserStream'
-      @client.user do |status|
-        next unless status['text']
+      loop do
+        @client = UserStream.client
+        puts 'Start UserStream'
+        begin
+          @client.user do |status|
+            next unless status['text']
 
-        tweet = Tweet.new
-        tweet.id = status['id']
-        tweet.user_id = status['user']['id']
-        tweet.name = status['user']['name']
-        tweet.screen_name = status['user']['screen_name']
-        tweet.text = status['text']
-        tweet.in_reply_to_status_id = status['in_reply_to_status_id']
-        tweet.in_reply_to_screen_name = status['in_reply_to_screen_name']
-        tweet.created_at = Time.parse(status['created_at'])
-        tweet.save
+            tweet = Tweet.new
+            tweet.id = status['id']
+            tweet.user_id = status['user']['id']
+            tweet.name = status['user']['name']
+            tweet.screen_name = status['user']['screen_name']
+            tweet.text = status['text']
+            tweet.in_reply_to_status_id = status['in_reply_to_status_id']
+            tweet.in_reply_to_screen_name = status['in_reply_to_screen_name']
+            tweet.created_at = Time.parse(status['created_at'])
+            tweet.save
+          end
+        rescue
+          puts 'ERROR: retry...'
+        end
+        sleep 5
       end
     end
   end
