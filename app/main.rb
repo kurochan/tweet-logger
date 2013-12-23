050885fb698d143a6703f968544bef9959e6538c
kurochan <kuro@kurochan.org>
2013-12-23 14:31:00 +0900

    環境が指定されない時はデフォルトでdevelopmentにするようにした

--- app/main.rb
+++ app/main.rb
@@ -2,17 +2,18 @@ require 'yaml'
 require 'active_record'
 require File.expand_path(File.dirname(__FILE__) + '/twitter_logger.rb')
 
+ENV['ENV'] = 'development' unless ENV['ENV']
 CONFIG = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/config.yml'))
 $stdout.sync = true
 
-ActiveRecord::Base.establish_connection(CONFIG['db'][ENV['ENV'] ? ENV['ENV'] : 'development'])
+ActiveRecord::Base.establish_connection(CONFIG['db'][ENV['ENV']])
 ActiveRecord::Base.default_timezone = :local
 
 Dir[File.expand_path('../model', __FILE__) << '/*.rb'].each do |file|
   require file
 end
 
-puts "Start Logger: ENV = #{ENV['ENV'] ? ENV['ENV'] : 'development'}"
+puts "Start Logger: ENV = #{ENV['ENV']}"
 twitter_logger = TwitterLogger.new
 thread = twitter_logger.start
 thread.join
