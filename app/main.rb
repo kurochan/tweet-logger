14ea8934e252d7ed8ba983d2eaf333c5837f9cb0
kurochan <kuro@kurochan.org>
2013-12-23 13:31:26 +0900

    Herokuがdatabase.ymlを上書きするようなので、設定をconfig.ymlに統合

--- app/main.rb
+++ app/main.rb
@@ -5,8 +5,7 @@ require File.expand_path(File.dirname(__FILE__) + '/twitter_logger.rb')
 CONFIG = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/config.yml'))
 $stdout.sync = true
 
-db_config  = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/database.yml'))
-ActiveRecord::Base.establish_connection(db_config['db'][ENV['ENV'] ? ENV['ENV'] : 'development'])
+ActiveRecord::Base.establish_connection(CONFIG['db'][ENV['ENV'] ? ENV['ENV'] : 'development'])
 ActiveRecord::Base.default_timezone = :local
 
 Dir[File.expand_path('../model', __FILE__) << '/*.rb'].each do |file|
