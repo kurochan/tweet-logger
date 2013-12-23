48d702af3741676251b7de2f4d4cae30c2d1178b
kurochan <kuro@kurochan.org>
2013-12-23 19:47:59 +0900

    collect deleted tweet

--- app/twitter_logger.rb
+++ app/twitter_logger.rb
@@ -1,5 +1,6 @@
 require 'userstream'
 require 'time'
+require 'pp'
 
 class TwitterLogger
   attr_reader :client
@@ -21,18 +22,11 @@ class TwitterLogger
         puts 'Start UserStream'
         begin
           @client.user do |status|
-            next unless status['text']
-
-            tweet = Tweet.new
-            tweet.id = status['id']
-            tweet.user_id = status['user']['id']
-            tweet.name = status['user']['name']
-            tweet.screen_name = status['user']['screen_name']
-            tweet.text = status['text']
-            tweet.in_reply_to_status_id = status['in_reply_to_status_id']
-            tweet.in_reply_to_screen_name = status['in_reply_to_screen_name']
-            tweet.created_at = Time.parse(status['created_at'])
-            tweet.save
+            if status['text']
+              save_status status
+            elsif status['delete']
+              save_deleted_status status
+            end
           end
         rescue => e
           puts e.class
@@ -43,5 +37,27 @@ class TwitterLogger
         sleep 5
       end
     end
+  end
+
+  private
+  def save_status(status)
+    tweet = Tweet.new
+    tweet.id = status['id']
+    tweet.user_id = status['user']['id']
+    tweet.name = status['user']['name']
+    tweet.screen_name = status['user']['screen_name']
+    tweet.text = status['text']
+    tweet.in_reply_to_status_id = status['in_reply_to_status_id']
+    tweet.in_reply_to_screen_name = status['in_reply_to_screen_name']
+    tweet.created_at = Time.parse(status['created_at'])
+    tweet.save
+  end
+
+  def save_deleted_status(status)
+    deleted_tweet = DeletedTweet.new
+    deleted_tweet.id = status['delete']['status']['id']
+    deleted_tweet.user_id =status['delete']['status']['user_id']
+    deleted_tweet.deleted_at = Time.now
+    deleted_tweet.save
   end
 end
